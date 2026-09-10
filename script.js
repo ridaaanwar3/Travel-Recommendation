ab52d34857fec4a5d7e8098e58dbe129ed422759
Rida <ridaaanwar3@gmail.com>
2026-09-10 16:42:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+function showMessage(destination) {
 
+    alert(
+        "You selected " +
+        destination +
+        ". Get ready for an amazing travel experience!"
+    );
+
+}
+
+
+function submitForm(event) {
+
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+
+    alert(
+        "Thank you, " +
+        name +
+        "! Your message has been submitted."
+    );
+
+    document.querySelector("form").reset();
+
+}
